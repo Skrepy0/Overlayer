e35a2c5eba131da37e60c6611a76d9364ad4541f
Skrepy <155234406+Skrepy0@users.noreply.github.com>
2026-07-05 10:26:22 +0800

    feat(config): 添加"Overlayer 配置"按钮的位置设置项

--- src/main/java/com/skrepy/overlayer/Config.java
+++ src/main/java/com/skrepy/overlayer/Config.java
@@ -5,5 +5,42 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
+    private static final ModConfigSpec.IntValue titleScreenBtnXOffset = BUILDER.defineInRange("titleScreenBtnXOffset", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    private static final ModConfigSpec.IntValue titleScreenBtnYOffset = BUILDER.defineInRange("titleScreenBtnYOffset", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    private static final ModConfigSpec.IntValue optionsScreenBtnXOffset = BUILDER.defineInRange("optionsScreenBtnXOffset", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    private static final ModConfigSpec.IntValue optionsScreenBtnYOffset = BUILDER.defineInRange("optionsScreenBtnYOffset", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+    public static int getTitleScreenBtnXOffset() {
+        return titleScreenBtnXOffset.get();
+    }
+
+    public static void setTitleScreenBtnXOffset(int titleScreenBtnXOffset) {
+        Config.titleScreenBtnXOffset.set(titleScreenBtnXOffset);
+    }
+
+    public static int getTitleScreenBtnYOffset() {
+        return titleScreenBtnYOffset.get();
+    }
+
+    public static void setTitleScreenBtnYOffset(int titleScreenBtnYOffset) {
+        Config.titleScreenBtnYOffset.set(titleScreenBtnYOffset);
+    }
+
+    public static int getOptionsScreenBtnXOffset() {
+        return optionsScreenBtnXOffset.get();
+    }
+
+    public static void setOptionsScreenBtnXOffset(int optionsScreenBtnXOffset) {
+        Config.optionsScreenBtnXOffset.set(optionsScreenBtnXOffset);
+    }
+
+    public static int getOptionsScreenBtnYOffset() {
+        return optionsScreenBtnYOffset.get();
+    }
+
+    public static void setOptionsScreenBtnYOffset(int optionsScreenBtnYOffset) {
+        Config.optionsScreenBtnYOffset.set(optionsScreenBtnYOffset);
+    }
+
     static final ModConfigSpec SPEC = BUILDER.build();
 }
